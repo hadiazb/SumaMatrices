aa24274fa4c4e9232a0e46f6866c43af4bb81c7d
hadiazb <hadiazb@unal.edu.co>
2020-04-24 15:01:54 -0500

    Agregando más cositas

--- suma.js
+++ suma.js
@@ -7,7 +7,52 @@ var numeroColumnasDos = 2;
 var matrixUno = construirMatrix(numeroFilasUno, numeroColumnasUno);
 var matrixDos = construirMatrix(numeroFilasDos, numeroColumnasDos);
 
-sumaMatrix(matrixUno, matrixDos)
+// sumaMatrix(matrixUno, matrixDos)
+fnCreaTabla(5, 5);
+
+
+function fnCreaTabla(renglones, columnas) {
+    var tblBodyUno = document.getElementById('miTablaUno').getElementsByTagName('tbody')[0];
+    var idUno = 0;
+
+    var tblBodyDos = document.getElementById('miTablaDos').getElementsByTagName('tbody')[0];
+    var idDos = 0;
+
+    for (let i = 0; i < renglones; i++) {
+        var renglonUno = document.createElement('tr');
+        var renglonDos = document.createElement('tr');
+        for (let j = 0; j < columnas; j++) {
+            var celdaUno = document.createElement('td');
+            var celdaDos = document.createElement('td');
+            idUno++;
+            idDos++;
+            var oInputUno;
+            var oInputDos;
+            
+            oInputUno = document.createElement('input');
+            oInputUno.type = 'text';
+            oInputUno.id = `celdaUno_[${i}][${j}]`;
+
+            oInputDos = document.createElement('input');
+            oInputDos.type = 'text';
+            oInputDos.id = `celdaDos_[${i}][${j}]`;
+            
+            celdaUno.appendChild(oInputUno);
+            renglonUno.appendChild(celdaUno);
+
+            celdaDos.appendChild(oInputDos);
+            renglonDos.appendChild(celdaDos);
+        }
+        tblBodyUno.appendChild(renglonUno);
+        tblBodyDos.appendChild(renglonDos);
+    }
+
+    // return console.log(tblBodyUno);
+}
+
+
+
+
 
 function construirMatrix(x, y) {
     var x;
